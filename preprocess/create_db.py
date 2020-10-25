584959bbb973d61ee34a72a5ce6550bda1b9b559
Daichi Kudo <sakanactor624@gmail.com>
2020-10-25 14:35:02 +0900

    classで処理をまとめる

--- preprocess/create_db.py
+++ preprocess/create_db.py
@@ -5,6 +5,129 @@ import pandas as pd
 
 import config
 
+
+# 解析用のデータベースの作成
+class CreateDB:
+    def __init__(self, drug: 'dataframe', demo: 'dataframe', reac: 'dataframe'):
+        """JADERのオリジナルデータ
+            dataframeの形で出力してください
+        """
+        self.drug = drug
+        self.demo = demo
+        self.reac = reac
+
+    def get_suspicious(self):
+        """被疑薬のみの抽出"""
+        sus_drug = self.drug[
+            drug["医薬品の関与"] == "被疑薬"
+        ]['識別番号', '医薬品連番', '医薬品（一般名）', '使用理由']
+        return sus_drug
+
+    def check_tnfa(self, sus_drug):
+        """TNF-α阻害薬に分類される5剤（インフリキシマブ、エタネルセプト、アダリズマブ、ゴリズマブ、セルトリズマブ ぺゴル）の選択 
+
+        該当の5剤、TNF-α阻害薬の服用の有無に対しフラグをつける
+        """
+        is_infliximab = (
+            (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）") |
+            (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続１］") |
+            (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続３］") |
+            (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続２］") |
+            (sus_drug["医薬品（一般名）"] == "INFLIXIMAB") |
+            (sus_drug["医薬品（一般名）"] == "ＩＮＦＬＩＸＩＭＡＢ") |
+            (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［インフリキシマブ後続３］") |
+            (sus_drug["医薬品（一般名）"] == "INFLIXIMAB (NGX)")
+        )
+        is_etanercept = (
+            (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）") |
+            (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）［後続１］") |
+            (sus_drug["医薬品（一般名）"] == "ETANERCEPT (NGX)") |
+            (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）［後続２］") |
+            (sus_drug["医薬品（一般名）"] == "エタネルセプト") |
+            (sus_drug["医薬品（一般名）"] == "ETANERCEPT") |
+            (sus_drug["医薬品（一般名）"] == "etanercept")
+        )
+        is_adalimumab = (
+            (sus_drug["医薬品（一般名）"] == "ADALIMUMAB(GENETICAL RECOMBINATION)") |
+            (sus_drug["医薬品（一般名）"] == "Adalimumab") |
+            (sus_drug["医薬品（一般名）"] == "アダリムマブ（遺伝子組換え）") |
+            (sus_drug["医薬品（一般名）"] == "アダリムマブ") |
+            (sus_drug["医薬品（一般名）"] == "Humira")
+        )
+        is_golimumab = (
+            (sus_drug["医薬品（一般名）"] == "ゴリムマブ（遺伝子組換え）") |
+            (sus_drug["医薬品（一般名）"] == "ゴリムマブ")
+        )
+        is_certolizumab = (
+            (sus_drug["医薬品（一般名）"] == "セルトリズマブペゴル") |
+            (sus_drug["医薬品（一般名）"] == "セルトリズマブ　ペゴル（遺伝子組換え）") |
+            (sus_drug["医薬品（一般名）"] == "ＣＥＲＴＯＬＩＺＵＭＡＢ　ＰＥＧＯＬ")
+        )
+        sus_drug['Infliximab'] = is_infliximab
+        sus_drug['Etanercept'] = is_etanercept
+        sus_drug['Adalimumab'] = is_adalimumab
+        sus_drug['Golimumab'] = is_golimumab
+        sus_drug['Certolizumab'] = is_certolizumab
+        sus_drug = sus_drug.replace(False, 0).replace(True, 1)
+        is_tnf = (
+            (sus_drug['Infliximab'] == 1) |
+            (sus_drug['Etanercept'] == 1) |
+            (sus_drug['Golimumab'] == 1) |
+            (sus_drug['Adalimumab'] == 1) |
+            (sus_drug['Certolizumab'] == 1)
+        )
+        sus_drug["is_tnf"] = is_tnf
+        sus_drug.drop(['医薬品連番', '医薬品（一般名）', '使用理由'], axis=1, inplace=True)
+
+        # 重複削除
+        sus_drug = sus_drug.drop_duplicates().replace(False, 0).replace(True, 1)
+        return sus_drug
+
+    def groupby_tnfa(self, sus_drug):
+        """TNF-α阻害薬を服用した患者を1人1レコードへ変換"""
+        sus_drug[sus_drug["is_tnf"] == 1].groupby("識別番号").max().to_csv(
+            'target/tnf_druger.csv', encoding='shift-jis'
+        )
+
+    def join_tnfa_and_demo_reac(self):
+        """drug, demo, jaderの結合
+        drug, demo, jaderを結合し、csvファイルとして保存する
+        """
+        drug = pd.read_csv('target/tnf_druger.csv', encoding='shift-jis')
+
+        # 重複削除
+        self.demo.drop_duplicates(inplace=True)
+        outer_drug_demo = pd.merge(
+            demo[['識別番号', '性別', '年齢', '報告年度・四半期']], drug, on="識別番号", how='outer'
+        )
+
+        # 欠損値の0埋め
+        outer_drug_demo[
+            ['Infliximab', 'Etanercept', 'Adalimumab',
+                'Golimumab', 'Certolizumab', 'is_tnf']
+        ].fillna(0)
+        outer_drug_demo = pd.concat(
+            [outer_drug_demo[['識別番号', '性別', '年齢', '報告年度・四半期']],
+             outer_drug_demo[
+                ['Infliximab', 'Etanercept', 'Adalimumab',
+                    'Golimumab', 'Certolizumab', 'is_tnf']
+            ].fillna(0)], axis=1)
+
+        # 重複削除
+        jader = pd.merge(
+            self.reac[['識別番号', '有害事象']].drop_duplicates(inplace=True),
+            outer_drug_demo, on='識別番号', how='outer'
+        )
+        # ベースデータの保存
+        jader.to_csv('target/data.csv', encoding='shift-jis')
+
+    def get_se(self):
+        """tnfαを飲んだ人の有害事象の集計データの保存"""
+        jader = pd.read_csv('target/data.csv', encoding='shift-jis')
+        jader[jader.is_tnf == 1].groupby('有害事象').sum().sort_values('is_tnf', ascending=False)[
+            'is_tnf'].to_csv('target/count_side_effect.csv', encoding='shift-jis')
+
+
 with codecs.open('jader/drug202008.csv', "r", "Shift-JIS", "ignore") as file:
     drug = pd.read_table(file, delimiter=",")
 
@@ -15,118 +138,14 @@ with codecs.open('jader/reac202008.csv', "r", "Shift-JIS", "ignore") as file:
     reac = pd.read_table(file, delimiter=",")
 
 
-# drugデータの加工：
-# 被疑薬の選択
-# TNF-α阻害薬に分類される5剤（インフリキシマブ、エタネルセプト、アダリズマブ、ゴリズマブ、セルトリズマブ ぺゴル）を選択
-# 1人1レコードへ変換
-def create_drug_df():
-    sus_drug = drug[drug["医薬品の関与"] == "被疑薬"]
-
-    sus_drug = sus_drug[['識別番号', '医薬品連番', '医薬品（一般名）', '使用理由']]
-    is_infliximab = (
-        (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）") |
-        (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続１］") |
-        (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続３］") |
-        (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［後続２］") |
-        (sus_drug["医薬品（一般名）"] == "INFLIXIMAB") |
-        (sus_drug["医薬品（一般名）"] == "ＩＮＦＬＩＸＩＭＡＢ") |
-        (sus_drug["医薬品（一般名）"] == "インフリキシマブ（遺伝子組換え）［インフリキシマブ後続３］") |
-        (sus_drug["医薬品（一般名）"] == "INFLIXIMAB (NGX)")
-    )
-    is_etanercept = (
-        (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）") |
-        (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）［後続１］") |
-        (sus_drug["医薬品（一般名）"] == "ETANERCEPT (NGX)") |
-        (sus_drug["医薬品（一般名）"] == "エタネルセプト（遺伝子組換え）［後続２］") |
-        (sus_drug["医薬品（一般名）"] == "エタネルセプト") |
-        (sus_drug["医薬品（一般名）"] == "ETANERCEPT") |
-        (sus_drug["医薬品（一般名）"] == "etanercept")
-    )
-    is_adalimumab = (
-        (sus_drug["医薬品（一般名）"] == "ADALIMUMAB(GENETICAL RECOMBINATION)") |
-        (sus_drug["医薬品（一般名）"] == "Adalimumab") |
-        (sus_drug["医薬品（一般名）"] == "アダリムマブ（遺伝子組換え）") |
-        (sus_drug["医薬品（一般名）"] == "アダリムマブ") |
-        (sus_drug["医薬品（一般名）"] == "Humira")  # 追加
-    )
-    is_golimumab = (
-        (sus_drug["医薬品（一般名）"] == "ゴリムマブ（遺伝子組換え）") |
-        (sus_drug["医薬品（一般名）"] == "ゴリムマブ")
-    )
-    is_certolizumab = (
-        (sus_drug["医薬品（一般名）"] == "セルトリズマブペゴル") |
-        (sus_drug["医薬品（一般名）"] == "セルトリズマブ　ペゴル（遺伝子組換え）") |
-        (sus_drug["医薬品（一般名）"] == "ＣＥＲＴＯＬＩＺＵＭＡＢ　ＰＥＧＯＬ")  # 追加
-    )
-
-    sus_drug['Infliximab'] = is_infliximab
-    sus_drug['Etanercept'] = is_etanercept
-    sus_drug['Adalimumab'] = is_adalimumab
-    sus_drug['Golimumab'] = is_golimumab
-    sus_drug['Certolizumab'] = is_certolizumab
-    sus_drug = sus_drug.replace(False, 0).replace(True, 1)
-
-    is_tnf = (
-        (sus_drug['Infliximab'] == 1) |
-        (sus_drug['Etanercept'] == 1) |
-        (sus_drug['Golimumab'] == 1) |
-        (sus_drug['Adalimumab'] == 1) |
-        (sus_drug['Certolizumab'] == 1)
-    )
-
-    sus_drug["is_tnf"] = is_tnf
-    sus_drug.drop(['医薬品連番', '医薬品（一般名）', '使用理由'], axis=1, inplace=True)
-
-    # 重複削除: Infliximab,Etanercept, Golimumab, Adalimumab,
-    sus_drug = sus_drug.drop_duplicates().replace(False, 0).replace(True, 1)
-
-    # tnfα阻害薬を飲んだ患者データを1人1レコードにして保存
-    sus_drug[sus_drug["is_tnf"] == 1].groupby("識別番号").max().to_csv(
-        'target/tnf_druger.csv', encoding='shift-jis'
-    )
+def main():
+    c = CreateDB(drug, demo, reac)
+    sus_drug = c.get_suspicious()
+    checked_drug = c.check_tnfa(sus_drug)
+    c.groupby_tnfa(checked_drug)
+    c.join_tnfa_and_demo_reac()
+    c.get_se()
 
 
-# 上記の加工済drugとdemo、reacの結合
-def join_df():
-    drug = pd.read_csv('target/tnf_druger.csv', encoding='shift-jis')
-
-    # 重複削除
-    demo.drop_duplicates(inplace=True)
-    outer_drug_demo = pd.merge(
-        demo[['識別番号', '性別', '年齢', '報告年度・四半期']], drug, on="識別番号", how='outer'
-    )
-
-    # 欠損値の0埋め
-    outer_drug_demo[
-        ['Infliximab', 'Etanercept', 'Adalimumab',
-            'Golimumab', 'Certolizumab', 'is_tnf']
-    ].fillna(0)
-    # concat 横結合：レコード数が同じことに注意
-    outer_drug_demo = pd.concat([outer_drug_demo[['識別番号', '性別', '年齢', '報告年度・四半期']], outer_drug_demo[[
-        'Infliximab', 'Etanercept', 'Adalimumab', 'Golimumab', 'Certolizumab', 'is_tnf']].fillna(0)], axis=1)
-
-    # 重複削除
-    jader = pd.merge(
-        reac[['識別番号', '有害事象']].drop_duplicates(inplace=True),
-        outer_drug_demo, on='識別番号', how='outer'
-    )
-    # *------------------*
-    # ベースデータの保存
-    jader.to_csv('target/data.csv', encoding='shift-jis')
-
-
-# 残りの解析用データの保存
-def store_data():
-    jader = pd.read_csv('target/data.csv', encoding='shift-jis')
-
-    # 年度ごとの集計データの保存
-    jader[jader.is_tnf == 1].groupby('報告年度・四半期').sum().to_csv(
-        'target/count_years.csv', encoding='shift-jis')
-
-    # tnfαを飲んだ人の有害事象の集計データの保存
-    jader[jader.is_tnf == 1].groupby('有害事象').sum().sort_values('is_tnf', ascending=False)[
-        'is_tnf'].to_csv('target/count_side_effect.csv', encoding='shift-jis')
-
-
-create_drug_df()
-store_data()
+if __name__ == "__main__":
+    main()
--- preprocess/ror.py
+++ preprocess/ror.py
@@ -5,36 +5,47 @@ import pandas as pd
 
 import config
 
-jader = pd.read_csv('target/data.csv', encoding='shift-jis')
 
-# 書き込み用CSVファイルの作成する
-with open('target/ror.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['drug', 'side_effect', 'a', 'b',
-                     'c', 'd', 'ror', '95%CI+', '95%CI-'])
+class ROR:
+    def __init__(self, jader=pd.read_csv('target/data.csv', encoding='shift-jis')):
+        self.jader = jader
 
-
-# 有害事象と薬ごとにRORを算出し、CSVファイルに書き込む
-def ror(df, side_effect, drug):
-    total = df['識別番号'].nunique()
-    try:
-        a = df[(df['有害事象'] == side_effect) & (df[drug] == 1)]['識別番号'].nunique()
-        b = df[df[drug] == 1]['識別番号'].nunique() - a
-        c = df[(df['有害事象'] == side_effect) & (df[drug] != 1)]['識別番号'].nunique()
-        d = total - (a+b+c)
-        ROR = (a/b)/(c/d)
-        ci_95m = math.exp(math.log(ROR)-1.96*(1/a+1/b+1/c+1/d)**0.5)
-        ci_95p = math.exp(math.log(ROR)+1.96*(1/a+1/b+1/c+1/d)**0.5)
-
-        # 追記モード
-        with open('target/ror.csv', 'a', newline='') as f:
+    def create_ror_file(self):
+        with open('target/ror.csv', 'w', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(
-                [drug, side_effect, a, b, c, d, ROR, ci_95m, ci_95p])
-        print(a, b, c, d)
-        print(ROR, ci_95m, ci_95p)
-    except ValueError:
-        print('バグった', side_effect, drug)
+            writer.writerow(['drug', 'side_effect', 'a', 'b',
+                             'c', 'd', 'ror', '95%CI+', '95%CI-'])
+
+    def ror(self, side_effect, drug):
+        """TNFα阻害薬と有害事象によるRORの算出とcsvファイルへの書き込み"""
+        total = self.jader['識別番号'].nunique()
+        try:
+            a = self.jader[(self.jader['有害事象'] == side_effect) & (
+                self.jader[drug] == 1)]['識別番号'].nunique()
+            b = self.jader[self.jader[drug] == 1]['識別番号'].nunique() - a
+            c = self.jader[(self.jader['有害事象'] == side_effect) & (
+                self.jader[drug] != 1)]['識別番号'].nunique()
+            d = total - (a+b+c)
+            ROR = (a/b)/(c/d)
+            ci_95m = math.exp(math.log(ROR)-1.96*(1/a+1/b+1/c+1/d)**0.5)
+            ci_95p = math.exp(math.log(ROR)+1.96*(1/a+1/b+1/c+1/d)**0.5)
+
+            # 追記モード
+            with open('target/ror.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(
+                    [drug, side_effect, a, b, c, d, ROR, ci_95m, ci_95p])
+            print(a, b, c, d, ROR, ci_95m, ci_95p)
+        except ValueError:
+            print('バグった', side_effect, drug)
+
+    def get_se_list(self, drug, num=30):
+        """各薬の上位30の有害事象の出力"""
+        return list(
+            self.jader[self.jader.is_tnf == 1].groupby('有害事象').sum().sort_values(
+                drug, ascending=False
+            )[drug].index[:num]
+        )
 
 
 drug_list = [
@@ -47,16 +58,15 @@ drug_list = [
 ]
 
 
-# 各薬のトップ30の有害事象を出力する
-def get_se_list(drug):
-    return list(
-        jader[jader.is_tnf == 1].groupby('有害事象').sum().sort_values(
-            drug, ascending=False
-        )[drug].index[:30]
-    )
+def main():
+    r = ROR()
+    r.create_ror_file()
+
+    # 有害事象と薬の全パタ ーンを実行する
+    for d in drug_list:
+        for s in r.get_se_list(d):
+            r.ror(s, d)
 
 
-# 有害事象と薬の全パターンを実行する
-for d in drug_list:
-    for s in get_se_list(d):
-        ror(jader, s, d)
+if __name__ == "__main__":
+    main()
